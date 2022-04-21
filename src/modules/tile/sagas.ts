4a821b13d667a0e4232f962fde5118b9979d75d3
beland.dev <>
2022-04-21 16:35:21 +0700

    update title api

--- src/modules/tile/sagas.ts
+++ src/modules/tile/sagas.ts
@@ -3,7 +3,7 @@ import { takeEvery, call, put } from 'redux-saga/effects'
 import { Atlas, AtlasTile } from '@beland/uikit'
 import { FETCH_TILES_REQUEST, FetchTilesRequestAction, fetchTilesSuccess, fetchTilesFailure } from './actions'
 
-export const MARKETPLACE_URL = env.get('REACT_APP_MARKETPLACE_URL', '')
+export const MARKETPLACE_URL = env.get('REACT_APP_HUB_SERVER_URL', '')
 
 export function* tileSaga() {
   yield takeEvery(FETCH_TILES_REQUEST, handleFetchTilesRequest)
@@ -11,7 +11,7 @@ export function* tileSaga() {
 
 function* handleFetchTilesRequest(_action: FetchTilesRequestAction) {
   try {
-    const tiles: Record<string, AtlasTile> = yield call(() => Atlas.fetchTiles(MARKETPLACE_URL + '/tiles'))
+    const tiles: Record<string, AtlasTile> = yield call(() => Atlas.fetchTiles('https://api.beland.io/v1/tiles'))
     yield put(fetchTilesSuccess(tiles))
   } catch (error) {
     yield put(fetchTilesFailure(error.message))
