76c14a46d150014ec5b11bd62a849176a69ba93c
beland.dev <beland.dev>
2022-06-06 09:01:30 +0700

    load authorizations

--- src/modules/wallet/sagas.ts
+++ src/modules/wallet/sagas.ts
@@ -13,6 +13,8 @@ import {
 import { fetchAuthorizationsRequest } from '@beland/dapps/dist/modules/authorization/actions'
 import { Authorization } from '@beland/dapps/dist/modules/authorization/types'
 import { TRANSACTIONS_API_URL } from './utils'
+import { buildBeanAuthorization } from 'lib/bean'
+import address from 'config/constants/contracts'
 
 const baseWalletSaga = createWalletSaga({
   CHAIN_ID: env.get('REACT_APP_CHAIN_ID') || ChainId.KAI_MAINNET,
@@ -31,20 +33,16 @@ function* customWalletSaga() {
   yield takeEvery(CHANGE_NETWORK, handleWalletChange)
 }
 
-function* handleWalletChange(_action: ConnectWalletSuccessAction | ChangeAccountAction | ChangeNetworkAction) {
-  // const { wallet } = action.payload
-  // const chainId = wallet.networks.KAI.chainId
+function* handleWalletChange(action: ConnectWalletSuccessAction | ChangeAccountAction | ChangeNetworkAction) {
+  const { wallet } = action.payload
+  const chainId = wallet.networks.KAI.chainId
   // All authorizations to be fetched must be added to the following list
   const authorizations: Authorization[] = []
 
   try {
-    // if (env.get('REACT_APP_FF_WEARABLES')) {
-    //   authorizations.push(buildManaAuthorization(wallet.address, chainId, ContractName.CollectionManager))
-    // }
-
-    // if (env.get('REACT_APP_FF_THIRD_PARTY_WEARABLES')) {
-    //   authorizations.push(buildManaAuthorization(wallet.address, chainId, ContractName.ThirdPartyRegistry))
-    // }
+    if (env.get('REACT_APP_FF_WEARABLES')) {
+      authorizations.push(buildBeanAuthorization(wallet.address, chainId, address.factory[24]))
+    }
 
     yield put(fetchAuthorizationsRequest(authorizations))
   } catch (error) {}
